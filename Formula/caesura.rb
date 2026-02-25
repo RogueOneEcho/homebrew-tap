634956ba5bb5d48ffccc1eaf5a859a6e4bb0782b
RogueOneEcho <RogueOneEcho@users.noreply.github.com>
2026-02-25 19:11:58 +0000

    Update caesura to 0.27.0

--- Formula/caesura.rb
+++ Formula/caesura.rb
@@ -1,31 +1,37 @@
 class Caesura < Formula
   desc "CLI for transcoding FLAC audio and uploading to Gazelle-based trackers"
   homepage "https://github.com/RogueOneEcho/caesura"
-  version "0.26.0"
+  version "0.27.0"
   license "AGPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.26.0/caesura-0.26.0-aarch64-apple-darwin"
-      sha256 "52a266dd87bc9f78e13658f7b8eaa730bc3b0b94cb84cf7e9c2cbb12abce214b"
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.0/caesura-0.27.0-aarch64-apple-darwin.tar.xz"
+      sha256 "b378e074f82885735bce5cb67b66b2df909af6f0523f878a91493123ad5df50c"
     end
   end
   on_macos do
     on_intel do
-      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.26.0/caesura-0.26.0-x86_64-apple-darwin"
-      sha256 "902237414c7b8fb938cdb5f21c16a9fbed180a0ccf7a40f02ef8a8869cac877a"
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.0/caesura-0.27.0-x86_64-apple-darwin.tar.xz"
+      sha256 "05dbb8de523ec8920f21b2062418fa33e5be7441b6ded4f085d97ee33cee9611"
+    end
+  end
+  on_linux do
+    on_arm do
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.0/caesura-0.27.0-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "1d33e2281d277ec80c2ef63c9931a90d15a5c990c52120e54a67a121e901fb3c"
     end
   end
   on_linux do
     on_intel do
-      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.26.0/caesura-0.26.0-x86_64-unknown-linux-gnu"
-      sha256 "aa31fcdb54404e04ed0a5d346973c9fbbd0b3952c30324906418876ec1196961"
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.0/caesura-0.27.0-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "31ef6d1757efc0f7a51b75d48cddd9100f95b2306168cbd504254eae465e3912"
     end
   end
 
   depends_on "flac"
   depends_on "lame"
-  depends_on "sox"
+  depends_on "sox_ng"
 
   def install
     bin.install "caesura"
