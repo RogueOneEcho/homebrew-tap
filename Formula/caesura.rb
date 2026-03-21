7a2e739fd09803e8dd10694a57d57e557257a119
RogueOneEcho <RogueOneEcho@users.noreply.github.com>
2026-03-21 20:00:48 +0000

    Update caesura to 0.27.2

--- Formula/caesura.rb
+++ Formula/caesura.rb
@@ -1,31 +1,31 @@
 class Caesura < Formula
   desc "CLI for transcoding FLAC audio and uploading to Gazelle-based trackers"
   homepage "https://github.com/RogueOneEcho/caesura"
-  version "0.27.1"
+  version "0.27.2"
   license "AGPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.1/caesura-0.27.1-aarch64-apple-darwin.tar.xz"
-      sha256 "6c2a2bf5d1d07f7a4191781d3cf1a823c124937e158db77dcac19025a14c716d"
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.2/caesura-0.27.2-aarch64-apple-darwin.tar.xz"
+      sha256 "c1aee9d6516fe2351da60f125da97d95b8ce921785f5f19b15cc3d53f8c58a79"
     end
   end
   on_macos do
     on_intel do
-      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.1/caesura-0.27.1-x86_64-apple-darwin.tar.xz"
-      sha256 "c4627f8fd6539d1664d581cb10c216bc743f976151da88dcd6de8118fbdff504"
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.2/caesura-0.27.2-x86_64-apple-darwin.tar.xz"
+      sha256 "1ab1b4996d5d18ee0944d40ac839c64d72748c6a4711bc3cb54e0e3f3930ba21"
     end
   end
   on_linux do
     on_arm do
-      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.1/caesura-0.27.1-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "f664c51ec6333e5f593864137a14dc1968d74ae8e96f26d335a1f54fcbaccf9a"
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.2/caesura-0.27.2-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "4c29a46388d32fe1cffaa36daba8d087280811687bba1fd0978b2b7650cc7357"
     end
   end
   on_linux do
     on_intel do
-      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.1/caesura-0.27.1-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "206a91bbf8b6d0875e466070d498f7e368de2b4b4e2f69a70a5dd12730d34f73"
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.2/caesura-0.27.2-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "b7e438dcc1d4dafcbd051be61bce8f406f71ba9bdb84638571c436b7620cf04b"
     end
   end
 
