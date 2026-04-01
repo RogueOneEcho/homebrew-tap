574ef096055a03fcf7ceaca0a857c52e71cd9a2a
RogueOneEcho <RogueOneEcho@users.noreply.github.com>
2026-04-01 18:40:27 +0000

    Update caesura to 0.28.0

--- Formula/caesura.rb
+++ Formula/caesura.rb
@@ -1,31 +1,31 @@
 class Caesura < Formula
   desc "CLI for transcoding FLAC audio and uploading to Gazelle-based trackers"
   homepage "https://github.com/RogueOneEcho/caesura"
-  version "0.27.2"
+  version "0.28.0"
   license "AGPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.2/caesura-0.27.2-aarch64-apple-darwin.tar.xz"
-      sha256 "c1aee9d6516fe2351da60f125da97d95b8ce921785f5f19b15cc3d53f8c58a79"
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.28.0/caesura-0.28.0-aarch64-apple-darwin.tar.xz"
+      sha256 "bc2b97a154263e8d757c31caf5953b4ea4779bc422bcf8ed0c4fd3bac5dfc916"
     end
   end
   on_macos do
     on_intel do
-      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.2/caesura-0.27.2-x86_64-apple-darwin.tar.xz"
-      sha256 "1ab1b4996d5d18ee0944d40ac839c64d72748c6a4711bc3cb54e0e3f3930ba21"
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.28.0/caesura-0.28.0-x86_64-apple-darwin.tar.xz"
+      sha256 "d29ec7ace860c9d8c93ca1ba4431b2c950dce70376c0c5be5a8e192ec9b9a388"
     end
   end
   on_linux do
     on_arm do
-      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.2/caesura-0.27.2-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "4c29a46388d32fe1cffaa36daba8d087280811687bba1fd0978b2b7650cc7357"
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.28.0/caesura-0.28.0-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "ad3be564a473629c8d9c6cf9a3d5ec21d20565c1d4767e8c7396675bc5061930"
     end
   end
   on_linux do
     on_intel do
-      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.27.2/caesura-0.27.2-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "b7e438dcc1d4dafcbd051be61bce8f406f71ba9bdb84638571c436b7620cf04b"
+      url "https://github.com/RogueOneEcho/caesura/releases/download/v0.28.0/caesura-0.28.0-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "88cbd338762d79d4e9de9e42a8381fbf98d50e1d68b8b1c491444ad86e462cc2"
     end
   end
 
